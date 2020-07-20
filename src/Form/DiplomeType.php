0eefdf073386cc208535d8f30bc9d4b8e64634c8
zakariakribia <68127321+zakariakribia@users.noreply.github.com>
2020-07-20 15:45:05 +0100

    Update DiplomeType.php

--- src/Form/DiplomeType.php
+++ src/Form/DiplomeType.php
@@ -6,17 +6,17 @@ use App\Entity\Diplome;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class DiplomeType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('code')
-            ->add('fichier')
-            ->add('dateObtention')
-            ->add('dateDelivrance')
-            ->add('annee')
+            ->add('fichier', FileType::class,array('data_class'=>null))
+            ->add('annee',DateType::class,['widget'=>'single_text'])
             ->add('type')
             ->add('specialite')
         ;
