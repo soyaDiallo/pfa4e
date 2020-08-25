681a6a234b76c43f3f75688c4e36a15617f45c55
ACHA-WEB <AICHA@DESKTOP-MR4TALD>
2020-08-25 03:01:37 +0100

    etablissement profil

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,18 +4,14 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Service\Sendmail;
-
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function index(Sendmail $sendmail)
+    public function index()
     {
-        $sendmail->call("aichasidibe615@gmail.com");
-
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
