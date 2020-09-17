d57a958553c6b4d034c24374362dfb2539989de1
abdaliDahir <abdali.dahir@gmail.com>
2020-09-17 13:42:26 +0100

    laureat registartion form flow

--- config/bundles.php
+++ config/bundles.php
@@ -17,5 +17,4 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
-    Craue\FormFlowBundle\CraueFormFlowBundle::class => ['all' => true],
 ];
--- src/Form/LaureatRegistrationFormType.php
+++ src/Form/LaureatRegistrationFormType.php
@@ -15,121 +15,117 @@ class LaureatRegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        switch ($options['flow_step']) {
-			case 1:
-				$builder
-                    ->add('pseudo', null, [
-                        'attr' => [
-                            'placeholder' => 'Username',
-                        ],
-                        'label' => false
-                    ])
-                    ->add('nom', null, [
-                        'attr' => [
-                            'placeholder' => 'Last Name',
-                        ],
-                        'label' => false
-                    ])
-                    ->add('prenom', null, [
-                        'attr' => [
-                            'placeholder' => 'First Name',
-                        ],
-                        'label' => false
-                    ])
-                    ->add('email', null, [
-                        'attr' => [
-                            'placeholder' => 'Email Address',
-                        ],
-                        'label' => false
-                    ])
-                    ->add('plainPassword', PasswordType::class, [
-                        // instead of being set onto the object directly,
-                        // this is read and encoded in the controller
-                        'mapped' => false,
-                        'constraints' => [
-                            new NotBlank([
-                                'message' => 'Please enter a password',
-                            ]),
-                            new Length([
-                                'min' => 6,
-                                'minMessage' => 'Your password should be at least {{ limit }} characters',
-                                // max length allowed by Symfony for security reasons
-                                'max' => 4096,
-                            ]),
-                        ],
-                        'attr' => [
-                            'placeholder' => 'Email Address',
-                        ],
-                        'label' => false
-                    ]);
-				break;
-			case 2:
-				// This form type is not defined in the example.
-                $builder
-                ->add('addresse', null, [
-                    'attr' => [
-                        'placeholder' => 'Addresse',
-                    ],
-                    'label' => false
-                ])
-                ->add('telephone', null, [
-                    'attr' => [
-                        'placeholder' => '+212 XXX XXX XXX',
-                    ],
-                    'label' => false
-                ])
-                ->add('datenaissance')
-                ->add('photoUrl', null, [
-                    'attr' => [
-                        'placeholder' => 'Photo de Profile',
-                    ],
-                    'label' => false
-                ])
-                ->add('cinNumSejour', null, [
-                    'attr' => [
-                        'placeholder' => 'CIN',
-                    ],
-                    'label' => false
-                ])
-                ->add('lieuNaissance', null, [
-                    'attr' => [
-                        'placeholder' => 'Lieu de Naissance',
-                    ],
-                    'label' => false
-                ])
-                ->add('nationalite', null, [
-                    'attr' => [
-                        'placeholder' => 'Nationalité',
-                    ],
-                    'label' => false
-                ])
-                ->add('nomArabe', null, [
-                    'attr' => [
-                        'placeholder' => 'الاسم العائلى',
-                        'dir' => 'rtl'
-                    ],
-                    'label' => false
-                ])
-                ->add('prenomArabe', null, [
-                    'attr' => [
-                        'placeholder' => 'الاسم الشخصي',
-                        'dir' => 'rtl'
-                    ],
-                    'label' => false
-                ])
-                ->add('S\'inscrire', SubmitType::class, [
-                    'attr' => ['class' => 'btn btn-widest btn-tall btn-primary rounded-0 shadow-sm'],
-                ]);
-				break;
-		}
-        ;
+        
+    // This form type is not defined in the example.
+    $builder
+        ->add('pseudo',null, [
+            'attr' => [
+                'placeholder' => 'Username',
+                'class' => 'from-control form-control-lg'
+            ],
+            'label' => false
+        ])
+        ->add('nom',null, [
+            'attr' => [
+                'placeholder' => 'Nom',
+            ],
+            'label' => false
+        ])
+        ->add('prenom',null, [
+            'attr' => [
+                'placeholder' => 'Prenom',
+            ],
+            'label' => false
+        ])
+        ->add('email',null, [
+            'attr' => [
+                'placeholder' => 'Email',
+            ],
+            'label' => false
+        ])
+        ->add('plainPassword', PasswordType::class, [
+                // instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'mapped' => false,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a password',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
+            ], [
+            'attr' => [
+                'placeholder' => 'password',
+            ],
+            'label' => false
+            ])
+        ->add('addresse', null, [
+            'attr' => [
+                'placeholder' => 'Addresse',
+            ],
+            'label' => false
+        ])
+        ->add('telephone', null, [
+            'attr' => [
+                'placeholder' => '+212 XXX XXX XXX',
+            ],
+            'label' => false
+        ])
+        ->add('datenaissance')
+        ->add('photoUrl', null, [
+            'attr' => [
+                'placeholder' => 'Photo de Profile',
+            ],
+            'label' => false
+        ])
+        ->add('cinNumSejour', null, [
+            'attr' => [
+                'placeholder' => 'CIN',
+            ],
+            'label' => false
+        ])
+        ->add('lieuNaissance', null, [
+            'attr' => [
+                'placeholder' => 'Lieu de Naissance',
+            ],
+            'label' => false
+        ])
+        ->add('nationalite', null, [
+            'attr' => [
+                'placeholder' => 'Nationalité',
+            ],
+            'label' => false
+        ])
+        ->add('nomArabe', null, [
+            'attr' => [
+                'placeholder' => 'الاسم العائلى',
+                'dir' => 'rtl'
+            ],
+            'label' => false
+        ])
+        ->add('prenomArabe', null, [
+            'attr' => [
+                'placeholder' => 'الاسم الشخصي',
+                'dir' => 'rtl'
+            ],
+            'label' => false
+        ])
+        ->add('inscrire', SubmitType::class, [
+            'attr' => ['class' => 'btn btn-widest btn-tall btn-primary rounded-0 shadow-sm'],
+        ]); 
+
     }
     
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'flow_step' => 1,
             'data_class' => Laureat::class,
         ]);
     }
+
 }
